dc234fe33a19bbed327e8e9aa56bcd86fe3b3621
Yeiko <yeiko2003@gmail.com>
2023-06-08 22:55:50 -0600

    firebase

---
+++ functions/src/index.ts
@@ -0,0 +1,31 @@
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+// Firebase Admin SDK to access Firestore.
+admin.initializeApp();
+const db = admin.firestore();
+
+/**
+ * Create user in Firestore on user account creation through Firebase Auth.
+ */
+export const createUser = functions.auth.user().onCreate(async (user) => {
+  /**
+   * Cherry pick user data to only send
+   * what we actually need to the client.
+   */
+  const newUser = {
+    uid: user.uid,
+    email: user.email,
+    displayName: user.displayName,
+    providerData: user.providerData,
+  };
+
+  db.collection("users").doc(user.uid).set(newUser);
+});
+/**
+ * Delete user in Firestore on user account deletion through Firebase Auth.
+ */
+export const deleteUserDocument = functions.auth
+  .user()
+  .onDelete(async (user) => {
+    db.collection("users").doc(user.uid).delete();
+  });
--- src/app/components/bloque.tsx
+++ src/app/components/bloque.tsx
@@ -1,40 +1,7 @@
 
-import Image from 'next/image';
-
 export default function Photo() {
   return (
-    <section className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto flex flex-wrap">
-        <div className="flex w-full mb-20 flex-wrap">
-          <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">Master Cleanse Reliac Heirloom</h1>
-          <p className="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">W</p>
-        </div>
-        <div className="flex flex-wrap md:-m-2 -m-1">
-          <div className="flex flex-wrap w-1/2">
-            <div className="md:p-2 p-1 w-1/2">
-              <Image alt="gallery" className="w-full object-cover h-full object-center block" src="/bus1.jpg" width={500} height={300} />
-            </div>
-            <div className="md:p-2 p-1 w-1/2">
-              <Image alt="gallery" className="w-full object-cover h-full object-center block" src="/bus1.jpg" width={501} height={301} />
-            </div>
-            <div className="md:p-2 p-1 w-full">
-              <Image alt="gallery" className="w-full h-full object-cover object-center block" src="/bus1.jpg" width={600} height={360} />
-            </div>
-          </div>
-          <div className="flex flex-wrap w-1/2">
-            <div className="md:p-2 p-1 w-full">
-              <Image alt="gallery" className="w-full h-full object-cover object-center block" src="/bus2.jpg" width={601} height={361} />
-            </div>
-            <div className="md:p-2 p-1 w-1/2">
-              <Image alt="gallery" className="w-full object-cover h-full object-center block" src="/bus2.jpg" width={502} height={302} />
-            </div>
-            <div className="md:p-2 p-1 w-1/2">
-              <Image alt="gallery" className="w-full object-cover h-full object-center block" src="/bus2.jpg" width={503} height={303} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    <h1>Holas</h1>
+  )
 };
 
